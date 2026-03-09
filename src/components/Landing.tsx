a6d2fab94fc91cbf06c8281f3ddfff74f2f9a697
Vinay Varma Pericherla <vinay@Vinays-MacBook-Pro.local>
2026-03-09 13:15:14 -0400

    Update name to Vinay Varma Pericherla and title to Software Engineer

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,20 +8,20 @@ const Landing = ({ children }: PropsWithChildren) => {
         <div className="landing-container">
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
-            <h1>
-              VINAY
+            <h1 style={{ fontSize: "calc(2rem + 3vw)" }}>
+              VINAY VARMA
               <br />
-              <span>VARMA</span>
+              <span>PERICHERLA</span>
             </h1>
           </div>
           <div className="landing-info">
             <h3>A Software</h3>
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Engineer</div>
-              <div className="landing-h2-2">Developer</div>
+              <div className="landing-h2-2">Engineer</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Developer</div>
+              <div className="landing-h2-info">Engineer</div>
               <div className="landing-h2-info-1">Engineer</div>
             </h2>
           </div>
